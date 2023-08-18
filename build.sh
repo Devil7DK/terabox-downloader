0fd44f4833eb1bdf74b61b749b8f50d213bd9cfe
Devil7DK <dineshthangavel47@gmail.com>
2023-08-18 10:55:55 +0530

    Update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-HOSTNAME=gcr.io
-PROJECT=devil7softwares
+echo "Enter Hostname:"
+read HOSTNAME
+
 IMAGE=terabox-downloader
 
 COMMIT_ID=$(git log --format="%H" -n 1)
 DATE=$(date +%Y%m%d)
 
-REPOSITORY="${HOSTNAME}/${PROJECT}/${IMAGE}"
+REPOSITORY="${HOSTNAME}/${IMAGE}"
 TAG="${REPOSITORY}:latest"
 
 docker build . -t "${TAG}"
