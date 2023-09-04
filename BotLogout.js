03d5a4e980642a63c8163c73dd86243580f6bf5a
Devil7DK <dineshthangavel47@gmail.com>
2023-09-04 22:59:55 +0530

    Add support for specifying bot api server in logout

--- BotLogout.js
+++ BotLogout.js
@@ -3,5 +3,9 @@ import { Telegraf } from "telegraf";
 
 config()
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+    telegram: {
+        apiRoot: process.env.BOT_API_SERVER,
+    },
+})
 bot.telegram.logOut().then((success) => success ? console.log("Logged out") : console.error("Failed to logout")).catch(console.error);
