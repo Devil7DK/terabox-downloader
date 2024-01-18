1138fdf83f337322f958c58b4e9bb38e76913a7f
Devil7DK <dineshthangavel47@gmail.com>
2024-01-18 12:23:06 +0530

    Update script to cache the host name & to automatically restart the server

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
-echo "Enter Hostname:"
-read HOSTNAME
+# Read if docker_host file exists
+# If not, ask for hostname
+
+if [ -f docker_host ]; then
+    HOSTNAME=$(cat docker_host)
+    echo "Using Docker Hostname: ${HOSTNAME}"
+    echo "Delete docker_host file to change hostname"
+else
+    echo "Enter Docker Hostname:"
+    read HOSTNAME
+    echo "${HOSTNAME}" > docker_host
+fi
 
 IMAGE=terabox-downloader
 
@@ -11,3 +21,19 @@ TAG="${REPOSITORY}:latest"
 
 docker build . -t "${TAG}"
 docker push "${TAG}"
+
+# Read if ssh_host file exists
+# If not, ask for hostname
+
+if [ -f ssh_host ]; then
+    SSH_HOSTNAME=$(cat ssh_host)
+    echo "Using SSH Hostname: ${SSH_HOSTNAME}"
+    echo "Delete ssh_host file to change hostname"
+else
+    echo "Enter Hostname (With user if needed):"
+    read SSH_HOSTNAME
+    echo "${SSH_HOSTNAME}" > ssh_host
+fi
+
+# Execute "./restart.sh terabox-downloader" on remote host from "/srv/docker" directory
+ssh "${SSH_HOSTNAME}" "cd /srv/docker && ./restart.sh ${IMAGE}"
